512ac908ddec7d92b904a827526303f75501008f
Aleksey <alekseykurylev@yandex.ru>
2025-09-14 18:08:58 +0300

    fix: ui

--- src/app/okpd2/(catalog)/layout.tsx
+++ src/app/okpd2/(catalog)/layout.tsx
@@ -7,7 +7,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div>
       <TopSidebar>
         <div className="flex items-center justify-between gap-4">
-          <div className="text-xl font-semibold">ОКПД 2</div>
+          <div className="text-xl font-semibold">
+            <Link href="/okpd2">ОКПД 2</Link>
+          </div>
           <Link
             className="cursor-pointer rounded-full p-2 hover:bg-gray-100"
             href="/okpd2/search"
