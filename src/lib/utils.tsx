05bf399766a20c1bf6e659874d16e94bccc22154
Aleksey <alekseykurylev@yandex.ru>
2025-09-15 20:29:22 +0300

    feat: Dialog

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -1,3 +1,10 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
 export function highlightText(text: string, query: string | undefined) {
   if (!query) return text;
   const parts = text.split(new RegExp(`(${query})`, "gi"));
