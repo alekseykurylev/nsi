8c890299916b8b378c4a15e22b7795375050f118
Aleksey <alekseykurylev@yandex.ru>
2025-09-15 09:33:26 +0300

    feat: fetcher

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -12,3 +12,14 @@ export function highlightText(text: string, query: string | undefined) {
     ),
   );
 }
+
+export async function fetcher<T>(
+  input: RequestInfo,
+  init?: RequestInit,
+): Promise<T> {
+  const res = await fetch(input, init);
+  if (!res.ok) {
+    throw new Error(`Request failed: ${res.status}`);
+  }
+  return res.json();
+}
